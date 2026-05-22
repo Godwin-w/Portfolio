8aa66e501a34f59c235d96f2d5a8c3c68b039ca6
Godwin <godwinw40@gmail.com>
2026-05-22 17:15:23 +0530

    Add Render env variables

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -107,6 +107,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'Godwinw40@gmail.com'
 EMAIL_HOST_PASSWORD = 'skml otyp ztap jsrv' 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
